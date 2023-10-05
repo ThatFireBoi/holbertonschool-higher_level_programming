2522850b849dea4ce6f704aa84b0a0b8676855a8
ThatFireBoi <gcf2007@hotmail.com>
2023-10-04 21:55:33 -0400

    Corrections to shebang

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,10 @@
-#!/bin/usr/python3
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) - 1):
-        return (my_list)
-
-    copy = [x for x in my_list]
-    copy[idx] = element
-    return (copy)
+    if idx < 0:
+        return my_list
+    elif idx >= len(my_list):
+        return my_list
+    else:
+        copy = [x for x in my_list]
+        copy[idx] = element
+        return (copy)
