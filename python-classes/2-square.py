d5095515ac212e9e535a9fb57f5c920608d43e8e
ThatFireBoi <gcf2007@hotmail.com>
2023-10-11 22:42:34 -0400

    Class square, private instance size

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,7 +8,7 @@ class Square:
     def __init__(self, size=0):
         """Instantiation"""
         self.__size = size
-        if type(size) is not int:
+        if type(size) is not int:  # check if size not an integer
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >=0")
