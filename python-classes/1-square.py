6ba131bdc90f863e78a7c63681fbf51ea1df3cdd
ThatFireBoi <gcf2007@hotmail.com>
2023-10-11 22:38:07 -0400

    Edits to comments

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,5 +3,8 @@
 
 
 class Square:
+    """This class is empty"""
+
     def __init__(self, size):
+        """This is the instantiation method for the Square class"""
         self._Square__size = size
