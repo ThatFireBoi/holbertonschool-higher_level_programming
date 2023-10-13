acc1aadbd704309c770c4e8637044558d1e359e7
ThatFireBoi <gcf2007@hotmail.com>
2023-10-13 18:09:14 -0400

    Chnages to name function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -3,7 +3,7 @@
 
 
 def say_my_name(first_name, last_name=""):
-    """Prints a name.
+    """Print a name.
 
     Args:
         first_name (str): The first name to print.
@@ -11,10 +11,8 @@ def say_my_name(first_name, last_name=""):
     Raises:
         TypeError: If either of first_name or last_name are not strings.
     """
-    if type(first_name is not str):
-        raise TypeError('first_name must be a string')
-
-    if last_name is not str:
-        raise TypeError('last_name must be a string')
-
-    print('My name is {}{}'.format(first_name, last_name))
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
