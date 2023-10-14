9f907d870343e9da88cea043af6169637eb886c3
ThatFireBoi <gcf2007@hotmail.com>
2023-10-13 23:46:15 -0400

    Text indentation, and test cases

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-"""Defines a text-indentation function."""
+"""
+    This module will have a
+    function that will print
+    a text with 2 new lines after
+    each of these characters:
+    ". ? :"
+"""
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
-
-    Args:
-        text (string): The text to print.
-    Raises:
-        TypeError: If text is not a string.
     """
-    if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
+    Function that prints a text
+    with 2 new lines after each
+    of the chracters above.
+    First we check that what its
+    passed is a str, then proceed to check and print.
+    """
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    for let in range(len(text)):
+        if text[let] == '.' or text[let] == '?' or text[let] == ':':
+            print(text[let])
+            print()
+        elif text[let] == " " and text[let - 1] in ['.', '?', ':']:
             continue
-        c += 1
+        else:
+            print(text[let], end="")
