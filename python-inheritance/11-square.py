d577a943d54d5765b1813e83e02522a1a533c5d6
ThatFireBoi <gcf2007@hotmail.com>
2023-10-20 10:37:57 -0400

    Square #2, returns description

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle (9-rectangle.py)"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+
+    def __init__(self, size):
+        """Instantiation with size"""
+        self.integer_validator('size', size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Returns the area of the square"""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns the following square description:
+        [Square] <width>/<height>"""
+        return '[Square] {}/{}'.format(self.__size, self.__size)
