e58daf9de933276e510ff7bdff150b54d6863a26
ThatFireBoi <gcf2007@hotmail.com>
2023-10-23 11:14:38 -0400

    Public getter and setter

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -17,8 +17,48 @@ class Rectangle(Base):
             id (int): id of the rectangle
         """
         super().__init__(id)
+        #  self.if = id
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-        # self.id = id
+
+    @property
+    def width(self):
+        """Returns the width of the rectangle"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width of the rectangle"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Returns the height of the rectangle"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets the height of the rectangle"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Returns the x coordinate of the rectangle"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Sets the x coordinate of the rectangle"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Returns the y coordinate of the rectangle"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Sets the y coordinate of the rectangle"""
+        self.__y = value
