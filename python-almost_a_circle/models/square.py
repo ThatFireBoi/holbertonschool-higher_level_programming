8aa14201860970ae69c272445952f14cdb433d45
ThatFireBoi <gcf2007@hotmail.com>
2023-10-25 19:12:58 -0400

    Update Squared, added def to_dictionary

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -44,3 +44,7 @@ class Square(Rectangle):
         elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Returns a dictionary representation of the square"""
+        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
