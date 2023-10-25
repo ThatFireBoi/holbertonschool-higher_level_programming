0a5a5d2bf73b85443d44e23bf28383bd524d12c4
ThatFireBoi <gcf2007@hotmail.com>
2023-10-25 18:21:01 -0400

    Updated square by adding def update

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -34,3 +34,13 @@ class Square(Rectangle):
         """Sets the size of the square"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Updates the square"""
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
