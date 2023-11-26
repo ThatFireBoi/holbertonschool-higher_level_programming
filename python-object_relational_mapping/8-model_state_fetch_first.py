abbf31b2b5e5a1b9e120623089bfc16945174a06
ThatFireBoi <gcf2007@hotmail.com>
2023-11-26 19:57:51 -0400

    Prints first State obj from database hbtn_0e_6_usa

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Script that prints the first State object from the database hbtn_0e_6_usa."""
+
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State, Base
+
+if __name__ == "__main__":
+    """Create an engine."""
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+
+    """Create a configured class Session."""
+    Session = sessionmaker(bind=engine)
+
+    """Create a Session instance."""
+    session = Session()
+
+    """Querying is done via the Query API"""
+    state = session.query(State).order_by(State.id).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+
+    """Close the session."""
+    session.close()
