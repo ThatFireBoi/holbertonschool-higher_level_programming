2da151bbf6ac3cc9d5b441a31e98ebaa4b58a52f
ThatFireBoi <gcf2007@hotmail.com>
2023-11-26 18:32:15 -0400

    Changes to query

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,30 +7,25 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    try:
-        """Connect to a MySQL server."""
-        db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                             passwd=argv[2], db=argv[3])
+    """Connect to a MySQL server."""
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
 
-        """Create a cursor object."""
-        cur = db.cursor()
+    """Create a cursor object."""
+    cur = db.cursor()
 
-        """Execute the query."""
-        query = "SELECT * FROM states WHERE name LIKE BINARY %s ORDER BY states.id ASC"
-        cur.execute(query, (argv[4],))
+    """Execute the query."""
+    query = "SELECT * FROM states WHERE name LIKE BINARY \
+        %s ORDER BY states.id ASC"
 
-        """Print the results of the query."""
-        for row in cur.fetchall():
-            print(row)
+    cur.execute(query, (argv[4],))
 
-        """Close all cursors."""
-        cur.close()
+    """Print the results of the query."""
+    for row in cur.fetchall():
+        print(row)
 
-        """Close all databases."""
-        db.close()
+    """Close all cursors."""
+    cur.close()
 
-    except MySQLdb.Error as e:
-        try:
-            print("MySQL Error [{:d}]: {}".format(e.args[0], e.args[1]))
-        except IndexError:
-            print("MySQL Error: {}".format(str(e)))
+    """Close all databases."""
+    db.close()
